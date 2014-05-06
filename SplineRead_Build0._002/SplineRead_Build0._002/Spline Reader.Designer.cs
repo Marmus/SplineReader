3b9a6d615525ac1c2018a715cfbd1ceff1ef99ca
Mark <marmusman@yahoo.com>
2014-05-05 22:14:57 -0400

    Build v0.5 beta
    Sent to Salmo

---
+++ SplineRead_Build0._002/SplineRead_Build0._002/Spline Reader.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace SplineRead_Build0._002
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 70);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(682, 533);
+            this.textBox1.TabIndex = 2;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(279, 24);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(303, 23);
+            this.progressBar2.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.AcceptsTab = true;
+            this.textBox2.Location = new System.Drawing.Point(780, 72);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(710, 87);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.AcceptsReturn = true;
+            this.textBox3.AcceptsTab = true;
+            this.textBox3.Location = new System.Drawing.Point(780, 202);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(710, 401);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Raw Data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(780, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "FMB Splines Header (Copy and Paste into Mission File)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(783, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(255, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "FMB Spline Listing (Copy and Paste into Mission File)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1104, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(207, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "(currently static data except for ID number)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(93, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Restart program for each new file";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(612, 1);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(195, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Spline Reader v0.5 beta  - Team Fusion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1548, 675);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Spline Reader v0.5 beta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ SplineRead_Build0._002/SplineRead_Build0._002/Spline Reader.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+//
+//Team Fusion Map Making Department
+//by ATAG_Marmus 
+//
+//Build v0.5 May 5, 2014
+//
+//
+namespace SplineRead_Build0._002
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            ofd.ShowDialog();
+            //BinaryReader br = new BinaryReader(File.OpenRead(ofd.FileName));
+
+            FileStream headerin = new FileStream((ofd.FileName), FileMode.Open,
+            FileAccess.Read, FileShare.ReadWrite);
+            //Create a BinaryReader from the FileStream
+            BinaryReader br = new BinaryReader(headerin);
+            //Seek to the start of the file
+            br.BaseStream.Seek(0, SeekOrigin.Begin); //the 0 is the offset from start of file
+            //Read the Header Data from the file and store the values to the variables
+            int Header = br.ReadInt32();
+            int UnknownHdr01 = br.ReadInt32();
+            int UnknownHdr02 = br.ReadInt32();
+            int UnknownHdr03 = br.ReadInt32();
+            int UnknownHdr04 = br.ReadInt32();
+            int UnknownHdr05 = br.ReadInt32();
+            int NumSplines = br.ReadInt32();
+            //Display the data on the console
+            textBox1.AppendText("File Header: \t\t" + Header.ToString() + "\r\n");
+            textBox1.AppendText("Unknown Header 01: \t" + UnknownHdr01.ToString() + "\r\n");            
+            textBox1.AppendText("Unknown Header 02 :\t" + UnknownHdr02 + "\r\n");
+            textBox1.AppendText("Unknown Header 03 :\t" + UnknownHdr03 + "\r\n");
+            textBox1.AppendText("Unknown Header 04 :\t" + UnknownHdr04 + "\r\n");
+            textBox1.AppendText("Unknown Header 05 :\t" + UnknownHdr05 + "\r\n");
+            textBox1.AppendText("Number of Splines in File :\t" + NumSplines + "\r\n");
+                        //Close the stream and free the resources
+            br.Close(); //closes the instance that read file Header only
+            textBox1.AppendText("File Header Read!\r\n");
+            textBox1.AppendText("====================================================== \r\n");
+
+            //progress bar setup
+            progressBar2.Minimum = 1;
+            progressBar2.Maximum = NumSplines;
+
+
+            /*
+             * spline FMB format
+            [splines]
+                3257 0_SplineRoad 13/width 5
+            [0_SplineRoad]
+                311608.1 16291.853 20.00 null R 311608.1 16388.785 20.00 -1
+                311582.676 16681.414 20.00  L 311601.211 16887.653 20.00  R 311536.493 17160.57201 20.00 -1
+                311401.833 17691.95903 20.00  L 311268.878 18136.71803 20.00  R 311134.203 18571.91804 20.00 -1
+                311024.48 19347.84004 20.00  L 310888.819 20098.79207 20.00 R 311024.48 19347.84004 20.00 -1
+                310888.819 20098.79207 20.00  L 310803.095 20800.88509 20.00 R 310838.407 21646.0931 20.00 -1
+                310864.338 22439.09511 20.00  L 310909.206 23240.55013 20.00 R 310975.594 24246.51213 20.00 -1
+                310989.979 25237.81212 20.00  L 311068.544 26150.27513 20.00 R 311278.338 27189.34613 20.00 -1
+                311458.216 28190.15513 20.00  L 311697.355 29169.82414 20.00 R 311977.463 30334.98014 20.00 -1
+                312247.779 31453.51314 20.00  L 312533.61 32548.98814 20.00 R 312846.602 33791.85214 20.00 -1
+                313131.749 35034.70014 20.00  L 313465.864 36180.64504 20.00 null -1
+            */
+
+            FileStream fin = new FileStream((ofd.FileName), FileMode.Open,
+            FileAccess.Read, FileShare.ReadWrite); //open file again to do loops
+            //Create a BinaryReader from the FileStream
+            BinaryReader br2 = new BinaryReader(fin);
+            //Seek to the start of the file
+            //attempt to add a loop
+            int counter = NumSplines - 1;
+            int pos = 28; //position of first spline
+            
+            textBox2.AppendText("[splines]\r\n");
+            for (int i = 0; i <= counter; i++)
+            {
+                textBox1.AppendText("File Position" + pos + "\r\n");
+                br2.BaseStream.Seek(pos, SeekOrigin.Begin);
+                int DefPoints = br2.ReadInt32();
+                int UnknownData01 = br2.ReadInt32();
+                int UnknownData02 = br2.ReadInt32();
+                uint IDNumber = br2.ReadUInt32();
+                int UnknownData03 = br2.ReadInt32();
+                int unknownData04 = br2.ReadInt32();
+                int UnknownData05 = br2.ReadInt32();
+                int splineLength = br2.ReadInt32();
+                //start double
+                double Xp_Origin = br2.ReadDouble();
+                double Yp_Origin = br2.ReadDouble();
+                double xL_Origin = br2.ReadDouble();
+                double yL_Origin = br2.ReadDouble();
+                double xR_Origin = br2.ReadDouble();
+                double yR_Origin = br2.ReadDouble();
+                //float Xpn = br2.ReadSingle();
+                //int pass = i;
+                //textBox2.Text = "Pass" + i;
+                progressBar2.PerformStep();
+
+
+                //Display the data on the textBox1  
+                textBox1.AppendText("     \r\n");
+                textBox1.AppendText("Spline \t\t\t" + "no." + (i + 1) + "\r\n");
+                textBox1.AppendText("Spline Header Value :\t" + DefPoints + "\r\n");
+                textBox1.AppendText("Unknown 01 :\t\t" + UnknownData01 + "\r\n");
+                textBox1.AppendText("Unknown 02 :\t\t" + UnknownData02 + "\r\n");
+                textBox1.AppendText("Spline ID Number :\t\t" + IDNumber + "\r\n");
+                textBox1.AppendText("Unknown 03 :\t\t" + UnknownData03 + "\r\n");
+                textBox1.AppendText("Unknown 04 :\t\t" + unknownData04 + "\r\n");
+                textBox1.AppendText("Unknown 05 :\t\t" + UnknownData05 + "\r\n");
+                textBox1.AppendText("Spline Length :\t\t" + splineLength + "\r\n");
+                //start double
+                // below used to list coordinates
+                textBox1.AppendText("     \r\n");
+                textBox1.AppendText(" Origin Coordinates (Global)\r\n");
+                textBox1.AppendText("Origin X-Coord :\t" + Xp_Origin + "\r\n");
+                textBox1.AppendText("Origin Y-Coord :\t" + Yp_Origin + "\r\n");
+                textBox1.AppendText("Origin xL-Coord :\t" + xL_Origin + "\r\n");
+                textBox1.AppendText("Origin yL-Coord :\t" + yL_Origin + "\r\n");
+                textBox1.AppendText("Origin xR-Coord :\t" + xR_Origin + "\r\n");
+                textBox1.AppendText("Origin yR-Coord :\t" + yR_Origin + "\r\n");
+                
+                //following is to get into FMB spline format
+                //
+                //textBox1.AppendText("\r\n");
+               // textBox1.AppendText("Spline into FMB format\r\n");
+                //textBox1.AppendText("-------------------------------------------------------------------------------------------------------------------------------------------------------\r\n");
+                //textBox2.AppendText("[splines]\r\n");
+                textBox2.AppendText(IDNumber + " " + i + "_SplineRoad 13/width 5\r\n");
+                //textBox2.AppendText("\r\n");
+                textBox3.AppendText("[" + i + "_SplineRoad]\r\n");
+                textBox3.AppendText(Math.Round(Xp_Origin, 2) + " ");
+                textBox3.AppendText(Math.Round(Yp_Origin, 2) + " 20.00 ");
+                //next 2 lines commented out to replace value with NULL
+                //textBox3.AppendText("L " + Math.Round(xL_Origin, 2) + " ");
+                //textBox3.AppendText(Math.Round(yL_Origin, 2) + " 20.00 R ");
+                textBox3.AppendText("null R ");
+                textBox3.AppendText(Math.Round(xR_Origin, 2) + " ");
+                textBox3.AppendText(Math.Round(yR_Origin, 2) + " 20.00 -1\r\n");
+                
+
+
+
+                textBox1.AppendText("     \r\n");
+                textBox1.AppendText("Control Points (Local Coordinates) :\r\n");
+                int jpos = (splineLength - 1);
+                for (int j = 1; j <= jpos; j++)
+                {
+                    textBox1.AppendText("     \r\n");
+                    textBox1.AppendText("Control Point " + (j + 1) + " :\r\n");
+
+                    float XpnLocal = br2.ReadSingle();
+                    double Xpn = Math.Round((Xp_Origin + XpnLocal), 2);
+                    textBox1.AppendText("Xp" + (j + 1) + " :" + XpnLocal + "\r\n");
+
+                    float YpnLocal = br2.ReadSingle();
+                    double Ypn = Math.Round((Yp_Origin + YpnLocal), 2);
+                    textBox1.AppendText("Yp" + (j + 1) + " :" + YpnLocal + "\r\n");
+
+                    float XLnLocal = br2.ReadSingle();
+                    double XLn = Math.Round((xL_Origin + XLnLocal), 2);
+                    textBox1.AppendText("xL" + (j + 1) + " :" + XLnLocal + "\r\n");
+
+                    float YLnLocal = br2.ReadSingle();
+                    double YLn = Math.Round((yL_Origin + YLnLocal), 2);
+                    textBox1.AppendText("yL" + (j + 1) + " :" + YLnLocal + "\r\n");
+
+                    float XRnLocal = br2.ReadSingle();
+                    double XRn = Math.Round((xR_Origin + XRnLocal), 2);
+                    textBox1.AppendText("xR" + (j + 1) + " :" + XRnLocal + "\r\n");
+
+                    float YRnLocal = br2.ReadSingle();
+                    double YRn = Math.Round((yR_Origin + YRnLocal), 2);
+                    textBox1.AppendText("yR" + (j + 1) + " :" + YRnLocal + "\r\n");
+                    //
+                    //below is to get all control points into FMB spline format
+                    //
+                    textBox3.AppendText(Xpn + " ");
+                    textBox3.AppendText(Ypn + " 20.00 L ");
+                    textBox3.AppendText(XLn + " ");
+                    textBox3.AppendText(YLn + " 20.00 ");
+                    //add if statement here to get NULL value at last string
+                    if (j == jpos)
+                    {
+                        textBox3.AppendText("null -1\r\n");
+                    }
+                    else
+                    {
+                        textBox3.AppendText("R" + XRn + " ");
+                        textBox3.AppendText(YRn + " 20.00 -1\r\n");
+                    }
+                    
+                    /* Try this formula from Salmo (not implimented for v0.5)
+                     * x0 = D
+                        x1 = D + C /3
+                        x2 = D + 2C/3 + B /3
+                        x3 = D + C + B + A
+
+                        y0 = H
+                        y1 = H + G / 3
+                        y2 = H + 2G / 3 + F / 3
+                        y3 = H + G + F + E
+                     */
+                    
+
+                }
+                //
+                
+                textBox1.AppendText("-------------------------------------------------------------------------------------------------------------------------------------------------------\r\n");
+                textBox1.AppendText("\r\n");
+                
+                //advance psition pos to end of string with splineLength value
+                
+                pos = pos + 32 + (48 + (splineLength - 1) * (24));
+                
+            }
+
+
+            br2.Close();
+        }
+    }
+}
